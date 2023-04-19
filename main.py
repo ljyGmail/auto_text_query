fbd3354cccec4cb54df9af96ee2b77c8a36e1f58
ljy <liangjy881@gmail.com>
2023-04-19 14:40:19 +0900

    폴더안에 파일을 자동으로 읽는 방식으로 변경

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 import pyautogui
 import time
 from bs4 import BeautifulSoup
@@ -7,81 +6,72 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from pathlib import Path
+import os
+import pyperclip
 
-# 노트패트 활성화
-notepad = pyautogui.getWindowsWithTitle('notepad++')
+LAB_PATH = Path(
+    r'C:\Users\M2S\Desktop\Workspace\PythonWorkspace\lab_for_auto_text_query')
+print(Path.cwd())
+SRC_PATH = LAB_PATH / 'src'
+DEST_PATH = LAB_PATH / 'dest'
 
 SITE_URL = "https://apps.crossref.org/SimpleTextQuery"
-GET_RESULT_TIMEOUT = 60
+GET_RESULT_TIMEOUT = 1800
 RESULT_XPATH = '//*[@id="mainContent2"]/div/form/font/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr[5]/td/table'
 
-if (len(notepad) > 0):
-    print('notepad찾았음')
-    notepad_window = notepad[0]
-    notepad_window.activate()  # notepad 활성화
-    time.sleep(0.3)
-    notepad_window.maximize()  # notepad 최대화
-    print(notepad_window.title)
-    filename_regex = re.compile(r'PB\d{8}.txt')
-    mo = filename_regex.search(notepad_window.title)
-    if mo != None:
-        filename = mo.group()
-        print(filename)
-    else:
-        exit()
+chrome_options = Options()  # 크롬 브라우저 옵션 설정
+chrome_options.add_experimental_option('detach', True)
 
-    pyautogui.hotkey('ctrl', 'a')  # 텍스트 전체 선택
-    pyautogui.hotkey('ctrl', 'c')  # 텍스트 복사
+# 원본 텍스트 파일 리스트
+for filename in os.listdir(SRC_PATH):
+    print(filename)
+    with open(SRC_PATH / filename, encoding='UTF8') as fh:
+        content = fh.read()  # 원본 내용 가져오기
+        pyperclip.copy(content)  # 원본내용을 클릭보드에 복사
 
+        browser = webdriver.Chrome(
+            'chromedriver.exe', options=chrome_options)  # 브라운저 열기
+        browser.get(SITE_URL)
+
+        time.sleep(0.3)
+
+        pyautogui.write(['\t', '\t', '\t', '\t', '\t',])  # TAB을 5번 누르기
+        pyautogui.hotkey('ctrl', 'v')  # 복사된 원본 텍스트를 붙여놓기
+
+        for i in range(2):
+            time.sleep(0.2)
+            pyautogui.write(['\t', ' '])  # 체크박스 2개 체크
+
+        time.sleep(0.2)
+        pyautogui.write(['\t', 'enter'])  # 엔터 누르기
+
+    elem = WebDriverWait(browser, GET_RESULT_TIMEOUT).until(
+        EC.presence_of_element_located((By.XPATH, RESULT_XPATH)))  # 결과 올때까지 대기
+    time.sleep(1)
+    inner_html = elem.get_attribute('innerHTML')  # 결과의 HTML내용 가져오기
+
+    soup = BeautifulSoup(inner_html, 'html.parser')
+
+    result_text = ''  # 치러된 결과
+    rows = soup.select('tr.resultB')
+    print(f'lenghth of result: {len(rows)}')
+    for row in rows:
+        td = row.td
+        count = len(td.contents)
+        if count > 0:
+            item = td.contents[0].string.text.strip().replace(
+                '\n      ', '')
+            result_text += item + '\n'
+        if count > 2:
+            link = td.contents[2].string.text
+            result_text += link + '\n'
+        if count > 4:
+            bottom_id = td.contents[4].string.text
+            result_text += bottom_id + '\n'
+        result_text += '\n'
     time.sleep(0.5)
+    with open(DEST_PATH / filename, 'w', encoding='UTF-8') as fh:
+        fh.write(result_text)
 
-    chr_options = Options()
-    chr_options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(
-        './chromedriver.exe', options=chr_options)
-    browser.get(SITE_URL)
-
-    browser.maximize_window()
-    time.sleep(0.3)
-    pyautogui.write(['\t', '\t', '\t', '\t', '\t',])  # TAB 5번 누르기
-    pyautogui.hotkey('ctrl', 'v')  # 복사된 텍스트 붙여놓기
-    time.sleep(0.2)
-    pyautogui.write(['\t', ' '])  # 첫번째 체크박스 체크
-    time.sleep(0.2)
-    pyautogui.write(['\t', ' '])  # 첫번째 체크박스 체크
-    time.sleep(0.2)
-    pyautogui.write(['\t', 'enter'])  # Submit버튼 클릭
-
-    try:
-        elem = WebDriverWait(browser, GET_RESULT_TIMEOUT).until(
-            EC.presence_of_element_located((By.XPATH, RESULT_XPATH)))
-        time.sleep(1)
-        inner_html = elem.get_attribute('innerHTML')
-        print(inner_html)
-        soup = BeautifulSoup(inner_html, 'html.parser')
-        rows = soup.select('tr.resultB')
-        result_text = ''
-        for row in rows:
-            td = row.td
-            count = len(td.contents)
-            if count > 0:
-                item = td.contents[0].string.text.strip().replace(
-                    '\n      ', '')
-                result_text += item + '\n'
-            if count > 2:
-                link = td.contents[2].string.text
-                result_text += link + '\n'
-            if count > 4:
-                bottom_id = td.contents[4].string.text
-                result_text += bottom_id + '\n'
-            result_text += '\n'
-        time.sleep(0.5)
-        with open(f'result_files/{filename}', 'w', encoding='utf-8') as fh:
-            fh.write(result_text)
-    except:
-        pyautogui.alert('오류 발생')
-    finally:
-        pyautogui.alert('처리 완료')
-        browser.quit()
-else:
-    pyautogui.alert('Notepad++프로그램을 열어두세요!')
+    browser.quit()
