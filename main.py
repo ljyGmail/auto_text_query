09a863d9886223a23bba44fc3b4a27bd2e394ef3
ljy <liangjy881@gmail.com>
2023-04-21 13:54:12 +0900

    화면 기본 기능 구현

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ import pyperclip
 import logging
 from logging import handlers
 import traceback
-
-
-def mkdir_if_not_exist(path):
-    if not path.exists():
-        path.mkdir()
+import sys
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import (
+    QApplication,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QStatusBar,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 # 로그 셋팅
@@ -34,15 +42,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
-LAB_PATH = Path(r'../lab_for_auto_text_query')
-mkdir_if_not_exist(LAB_PATH)
-
-SRC_PATH = LAB_PATH / 'src'
-mkdir_if_not_exist(SRC_PATH)
-
-DEST_PATH = LAB_PATH / 'dest'
-mkdir_if_not_exist(DEST_PATH)
-
 SITE_URL = "https://apps.crossref.org/SimpleTextQuery"
 GET_RESULT_TIMEOUT = 1800
 RESULT_XPATH = '//*[@id="mainContent2"]/div/form/font/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr/td/table/tbody/tr[5]/td/table'
@@ -50,83 +49,155 @@ RESULT_XPATH = '//*[@id="mainContent2"]/div/form/font/table/tbody/tr/td/table/tb
 chrome_options = Options()  # 크롬 브라우저 옵션 설정
 chrome_options.add_experimental_option('detach', True)
 
-logging.info(f"{'#' * 10} 처리 시작 {'#' * 10}")
 
-regex = re.compile(r'[A-Za-z]{2}\d{8}.txt')
-file_list = os.listdir(SRC_PATH)
-logging.info(f'폴더내 파일 수: {len(file_list)}')
+class MainWindow(QMainWindow):
 
-file_list = list(filter(lambda x: regex.search(x), os.listdir(SRC_PATH)))
-file_number = len(file_list)
-logging.info(f'실제 처리할 파일 수: {file_number}')
+    def __init__(self):
+        super().__init__()
 
-# 원본 텍스트 파일 리스트
-for index, filename in enumerate(file_list):
-    try:
-        logging.info(f'==========> {filename} ({index + 1} / {file_number})')
+        self.setWindowTitle('Auto Text Query')
 
-        with open(SRC_PATH / filename, encoding='UTF8') as fh:
-            content = fh.read()  # 원본 내용 가져오기
-            pyperclip.copy(content)  # 원본내용을 클릭보드에 복사
+        layout = QVBoxLayout()
 
-            logging.info('원본 텍스트 복사됨')
+        src_row_layout = QHBoxLayout()
+        dest_row_layout = QHBoxLayout()
 
-            browser = webdriver.Chrome(
-                'chromedriver.exe', options=chrome_options)  # 브라운저 열기
-            browser.get(SITE_URL)
+        layout.addLayout(src_row_layout)
+        layout.addLayout(dest_row_layout)
 
-            time.sleep(0.3)
+        src_label = QLabel('원본 폴더 지정:')
+        src_btn = QPushButton('찾기')
+        src_btn.clicked.connect(self.get_folder_src)
+        self.src_path_label = QLabel('')
+        src_row_layout.addWidget(src_label)
+        src_row_layout.addWidget(src_btn)
+        src_row_layout.addWidget(self.src_path_label)
 
-            pyautogui.write(['\t', '\t', '\t', '\t', '\t',])  # TAB을 5번 누르기
-            pyautogui.hotkey('ctrl', 'v')  # 복사된 원본 텍스트를 붙여놓기
+        dest_label = QLabel('결과 폴더 지정:')
+        dest_btn = QPushButton('찾기')
+        dest_btn.clicked.connect(self.get_folder_dest)
+        self.dest_path_label = QLabel('')
+        dest_row_layout.addWidget(dest_label)
+        dest_row_layout.addWidget(dest_btn)
+        dest_row_layout.addWidget(self.dest_path_label)
 
-            for i in range(2):
-                time.sleep(0.2)
-                pyautogui.write(['\t', ' '])  # 체크박스 2개 체크
+        process_btn = QPushButton('시작')
+        process_btn.clicked.connect(self.start_process)
+        layout.addWidget(process_btn)
 
-            time.sleep(0.2)
-            pyautogui.write(['\t', 'enter'])  # 엔터 누르기
+        container = QWidget()
+        container.setLayout(layout)
 
-            logging.info('웹 화면에서 엔터 눌렀음')
+        self.setStatusBar(QStatusBar(self))
+        # self.setFixedSize(QSize(500, 250))
+        self.setCentralWidget(container)
 
-        elem = WebDriverWait(browser, GET_RESULT_TIMEOUT).until(
-            EC.presence_of_element_located((By.XPATH, RESULT_XPATH)))  # 결과 올때까지 대기
-        time.sleep(1)
+    def get_folder_src(self):
+        initial_dir = '../lab_for_auto_text_query'
+        folder_path = QFileDialog.getExistingDirectory(
+            self, directory=initial_dir)
+        self.src_path_label.setText(folder_path)
+        print(f'path for src:', folder_path)
 
-        logging.info('결과 조회 완료')
+    def get_folder_dest(self):
+        initial_dir = '../lab_for_auto_text_query'
+        folder_path = QFileDialog.getExistingDirectory(
+            self, directory=initial_dir)
+        self.dest_path_label.setText(folder_path)
+        print(f'path for dest:', folder_path)
 
-        inner_html = elem.get_attribute('innerHTML')  # 결과의 HTML내용 가져오기
+    def start_process(self):
+        print('###############################################')
+        logging.info(f"{'#' * 10} 처리 시작 {'#' * 10}")
+        src_path = self.src_path_label.text()
+        dest_path = self.dest_path_label.text()
 
-        soup = BeautifulSoup(inner_html, 'html.parser')
-        logging.info('HTML 파싱 완료')
+        regex = re.compile(r'[A-Za-z]{2}\d{8}.txt')
+        file_list = os.listdir(src_path)
+        logging.info(f'원본 폴더내 파일 수: {len(file_list)}')
 
-        result_text = ''  # 치러된 결과
-        rows = soup.select('tr.resultB')
-        logging.info(f'결과 항목 개수: {len(rows)}')
-        for row in rows:
-            td = row.td
-            count = len(td.contents)
-            if count > 0:
-                item = td.contents[0].string.text.strip().replace(
-                    '\n      ', '')
-                result_text += item + '\n'
-            if count > 2:
-                link = td.contents[2].string.text
-                result_text += link + '\n'
-            if count > 4:
-                bottom_id = td.contents[4].string.text
-                result_text += bottom_id + '\n'
-            result_text += '\n'
-        logging.debug(f'처리된 결과: {result_text}')
-        time.sleep(0.5)
+        file_list = list(
+            filter(lambda x: regex.search(x), os.listdir(src_path)))
+        file_number = len(file_list)
+        logging.info(f'실제 처리할 파일 수: {file_number}')
 
-        with open(DEST_PATH / filename, 'w', encoding='UTF-8') as fh:
-            fh.write(result_text)
-            logging.info('결과 텍스트를 파일에 쓰기 완료')
+        # 원본 텍스트 파일 리스트
+        for index, filename in enumerate(file_list):
+            try:
+                logging.info(
+                    f'==========> {filename} ({index + 1} / {file_number})')
 
-        browser.quit()
-        logging.info('브라우저 종료')
-    except:
-        logging.error(traceback.format_exc())
+                with open(Path(src_path) / filename, encoding='UTF8') as fh:
+                    content = fh.read()  # 원본 내용 가져오기
+                    pyperclip.copy(content)  # 원본내용을 클릭보드에 복사
 
-logging.info(f"{'#' * 10} 처리 완료 {'#' * 10}")
+                    logging.info('원본 텍스트 복사됨')
+
+                    browser = webdriver.Chrome(
+                        'chromedriver.exe', options=chrome_options)  # 브라운저 열기
+                    browser.get(SITE_URL)
+
+                    time.sleep(0.3)
+
+                    # TAB을 5번 누르기
+                    pyautogui.write(['\t', '\t', '\t', '\t', '\t',])
+                    pyautogui.hotkey('ctrl', 'v')  # 복사된 원본 텍스트를 붙여놓기
+
+                    for i in range(2):
+                        time.sleep(0.2)
+                        pyautogui.write(['\t', ' '])  # 체크박스 2개 체크
+
+                    time.sleep(0.2)
+                    pyautogui.write(['\t', 'enter'])  # 엔터 누르기
+
+                    logging.info('웹 화면에서 엔터 눌렀음')
+
+                elem = WebDriverWait(browser, GET_RESULT_TIMEOUT).until(
+                    EC.presence_of_element_located((By.XPATH, RESULT_XPATH)))  # 결과 올때까지 대기
+                time.sleep(1)
+
+                logging.info('결과 조회 완료')
+
+                inner_html = elem.get_attribute('innerHTML')  # 결과의 HTML내용 가져오기
+
+                soup = BeautifulSoup(inner_html, 'html.parser')
+                logging.info('HTML 파싱 완료')
+
+                result_text = ''  # 치러된 결과
+                rows = soup.select('tr.resultB')
+                logging.info(f'결과 항목 개수: {len(rows)}')
+                for row in rows:
+                    td = row.td
+                    count = len(td.contents)
+                    if count > 0:
+                        item = td.contents[0].string.text.strip().replace(
+                            '\n      ', '')
+                        result_text += item + '\n'
+                    if count > 2:
+                        link = td.contents[2].string.text
+                        result_text += link + '\n'
+                    if count > 4:
+                        bottom_id = td.contents[4].string.text
+                        result_text += bottom_id + '\n'
+                    result_text += '\n'
+                logging.debug(f'처리된 결과: {result_text}')
+                time.sleep(0.5)
+
+                with open(Path(dest_path) / filename, 'w', encoding='UTF-8') as fh:
+                    fh.write(result_text)
+                    logging.info('결과 텍스트를 파일에 쓰기 완료')
+
+                browser.quit()
+                logging.info('브라우저 종료')
+            except:
+                logging.error(traceback.format_exc())
+
+        logging.info(f"{'#' * 10} 처리 완료 {'#' * 10}")
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
